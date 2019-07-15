b7bf9b9ce41fab21cadba41ef990d1d6c9a82c43
Eddy Cortez <ecortezr@gmail.com>
2019-07-15 16:17:14 -0400

    First commit. Lesson 20. Creando las Screens

---
+++ app/utils/firebase.js
@@ -0,0 +1,9 @@
+export default (firebaseConfig = {
+  apiKey: "AIzaSyDw0yL0HWL_zapIJCdsgEHRqk1D4bOX06w",
+  authDomain: "tenedores-5.firebaseapp.com",
+  databaseURL: "https://tenedores-5.firebaseio.com",
+  projectId: "tenedores-5",
+  storageBucket: "tenedores-5.appspot.com",
+  messagingSenderId: "622540523004",
+  appId: "1:622540523004:web:5ec0f50ea2cc7234"
+});
