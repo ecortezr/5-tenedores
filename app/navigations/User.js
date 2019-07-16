961ba4b7805ae508b502cbb0121eb5a062a0a9ee
Eddy Cortez <ecortezr@gmail.com>
2019-07-15 21:41:29 -0400

    22. Terminando la Navegación

---
+++ app/navigations/User.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { Icon } from "react-native-elements";
+import {
+  createStackNavigator,
+  createAppContainer,
+  createBottomTabNavigator
+} from "react-navigation";
+
+// Screens
+import HomeScreen from "../screens/Home";
+import Top5Screen from "../screens/Top5";
+import SearchScreen from "../screens/Search";
+import MyAccountScreen from "../screens/MyAccount";
+
+const homeScreenStack = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: "Home"
+    })
+  }
+});
+
+const top5ScreenStack = createStackNavigator({
+  Top5: {
+    screen: Top5Screen,
+    navigationOptions: ({ navigation }) => ({
+      title: "Top 5 de Restaurantes"
+    })
+  }
+});
+
+const searchScreenStack = createStackNavigator({
+  Search: {
+    screen: SearchScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: "Buscar"
+    })
+  }
+});
+
+const myAccountScreenStack = createStackNavigator({
+  MyAccount: {
+    screen: MyAccountScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: "Mi Cuenta"
+    })
+  }
+});
+
+const RootStack = createBottomTabNavigator(
+  {
+    Home: {
+      screen: homeScreenStack,
+      navigationOptions: ({ navigation }) => ({
+        tabBarLabel: "Inicio",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon
+            type="material-community"
+            size={22}
+            color={tintColor}
+            name="compass-outline"
+          />
+        )
+      })
+    },
+    Top5: {
+      screen: top5ScreenStack,
+      navigationOptions: ({ navigation }) => ({
+        tabBarLabel: "Top 5",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon
+            type="material-community"
+            size={22}
+            color={tintColor}
+            name="star-outline"
+          />
+        )
+      })
+    },
+    Search: {
+      screen: searchScreenStack,
+      navigationOptions: ({ navigation }) => ({
+        tabBarLabel: "Buscar",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon
+            type="material-community"
+            size={22}
+            color={tintColor}
+            name="magnify"
+          />
+        )
+      })
+    },
+    MyAccount: {
+      screen: myAccountScreenStack,
+      navigationOptions: ({ navigation }) => ({
+        tabBarLabel: "Mi Cuenta",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon
+            type="material-community"
+            size={22}
+            color={tintColor}
+            name="home-outline"
+          />
+        )
+      })
+    }
+  },
+  {
+    initialRouteName: "Top5", // Opcional
+    order: ["Home", "Top5", "Search", "MyAccount"],
+    tabBarOptions: {
+      inactiveTintColor: "#646464",
+      activeTintColor: "#00a680"
+    }
+  }
+);
+
+export default createAppContainer(RootStack);
