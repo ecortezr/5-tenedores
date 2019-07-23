d658cf693851a22bb36f171d5cc9e8ce01a85903
Eddy Cortez <ecortezr@gmail.com>
2019-07-23 09:16:03 -0400

    65. Creando el formulario de nuevo restaurante

---
+++ app/forms/AddRestaurantForm.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import t from "tcomb-form-native";
+import inputTemplate from "./templates/Input";
+
+export const AddRestaurantStruct = t.struct({
+  name: t.String,
+  city: t.String,
+  address: t.String,
+  description: t.String
+});
+
+export const AddRestaurantOptions = {
+  fields: {
+    name: {
+      template: inputTemplate,
+      config: {
+        placeholder: "Nombre del Restaurant",
+        iconType: "material-community",
+        iconName: "silverware"
+      }
+    },
+    city: {
+      template: inputTemplate,
+      config: {
+        placeholder: "Ciudad",
+        iconType: "material-community",
+        iconName: "city"
+      }
+    },
+    address: {
+      template: inputTemplate,
+      config: {
+        placeholder: "Dirección",
+        iconType: "material-community",
+        iconName: "map-marker"
+      }
+    },
+    descripction: {}
+  }
+};
--- app/screens/Restaurants/addRestaurant.js
+++ app/screens/Restaurants/addRestaurant.js
@@ -1,21 +1,46 @@
-import React, { Component } from "react"
-import { StyleSheet, View, Text } from "react-native"
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+
+import t from "tcomb-form-native";
+import {
+  AddRestaurantOptions,
+  AddRestaurantStruct
+} from "../../forms/AddRestaurantForm";
+const Form = t.form.Form;
 
 export default class AddRestaurant extends Component {
+
+  constructor(){
+    super()
+
+    this.state = {
+      // Es opcional establecerle valores a cada campo. Puede ser un objeto vacío
+      formData = {
+        name: '',
+        city:'',
+        address:'',
+        description:''
+      }
+    }
+  }
   render() {
     return (
       <View style={styles.viewBody}>
-        <Text>Agregar restaurant...</Text>
+        <View>
+          <Form
+            ref="addRestaurantForm"
+            type={AddRestaurantStruct}
+            options={AddRestaurantOptions}
+            value={this.state.formData}
+          />
+        </View>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   viewBody: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#fff"
+    flex: 1
   }
-})
+});
