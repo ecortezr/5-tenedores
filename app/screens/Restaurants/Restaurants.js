f2f5f0679c2d530d5ba3a92776e417a39867038b
Eddy Cortez <ecortezr@gmail.com>
2019-07-23 15:59:11 -0400

    75. Ocultando el (+) de los usuarios no logeados

--- app/screens/Restaurants/Restaurants.js
+++ app/screens/Restaurants/Restaurants.js
@@ -1,24 +1,61 @@
-import React, { Component } from "react"
-import { StyleSheet, View, Text } from "react-native"
-import ActionButton from "react-native-action-button"
-import Icon from "react-native-vector-icons/Ionicons"
+import React, { Component } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import ActionButton from "react-native-action-button";
+import Icon from "react-native-vector-icons/Ionicons";
+
+import * as firebase from "firebase";
 
 export default class Restaurants extends Component {
-  goToScreen = nameScreen => {
-    console.log("goToScreen")
-    this.props.navigation.navigate(nameScreen)
+  constructor() {
+    super();
+
+    this.state = {
+      login: false
+    };
   }
+
+  componentDidMount() {
+    this.checkLogin();
+  }
+
+  checkLogin = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          login: true
+        });
+      } else {
+        this.setState({
+          login: false
+        });
+      }
+    });
+  };
+
+  goToScreen = nameScreen => {
+    console.log("goToScreen");
+    this.props.navigation.navigate(nameScreen);
+  };
+
+  loadActionButton = () => {
+    const { login } = this.state;
+    if (login) {
+      return (
+        <ActionButton
+          buttonColor="#00a680"
+          onPress={() => this.goToScreen("AddRestaurant")}
+        />
+      );
+    }
+  };
 
   render() {
     return (
       <View style={styles.viewBody}>
         <Text>Restaurantes Screen...</Text>
-        <ActionButton
-          buttonColor="#00a680"
-          onPress={() => this.goToScreen("AddRestaurant")}
-        />
+        {this.loadActionButton()}
       </View>
-    )
+    );
   }
 }
 
@@ -29,4 +66,4 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     backgroundColor: "#fff"
   }
-})
+});
