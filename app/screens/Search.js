cb80c9910e4eddf00c44ce25f0fc91a6dd045f93
Eddy Cortez <ecortezr@gmail.com>
2019-07-25 08:20:24 -0400

    101. Logica para sacar los Restaurantes relacionado con el text

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -2,19 +2,46 @@ import React, { Component } from "react"
 import { StyleSheet, View, Text } from "react-native"
 import { SearchBar } from "react-native-elements"
 
+import { firebaseApp } from "../utils/firebase"
+import firebase from "firebase/app"
+import "firebase/firestore"
+const db = firebase.firestore(firebaseApp)
+
+import { FireSQL } from "firesql"
+const fireSQL = new FireSQL(firebase.firestore(), { includeId: "id" })
+
 export default class Search extends Component {
   constructor() {
     super()
 
     this.state = {
-      search: ""
+      search: "",
+      searchResults: null
     }
   }
 
-  searchRestaurants = search => {
+  searchRestaurants = async search => {
     this.setState({
       search
     })
+    // Buscar en Firestore, usando Firesql
+    const restaurantQuery = fireSQL.query(`
+    select *
+    from restaurants
+    WHERE name LIKE '${search}%'
+    `)
+
+    let results = []
+    await restaurantQuery
+      .then(queryResults => {
+        results = queryResults
+      })
+      .catch(err => {})
+    this.setState({
+      searchResults: results
+    })
+
+    console.log(this.state)
   }
 
   render() {
